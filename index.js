617d1f97adcc9e673e85147c425a774503c828a2
Evan Lee <evan.lee.615@gmail.com>
2024-02-03 01:48:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 1;
+let num2 = 62;
+
+let multiply = num1*num2;
+
+
+let random = (Number.parseInt(Math.random()))+1;
+
+
+let num3 = 10;
+let num4 = 24;
+
+let mod = num4 % num3;
+
+
+let max = Math.max(10,20);
